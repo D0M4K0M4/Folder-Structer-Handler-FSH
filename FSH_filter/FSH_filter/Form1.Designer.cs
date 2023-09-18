c065c70c14ce7025374c3aac5bcef2e0703c0046
D0M4K0M4 <144862935+D0M4K0M4@users.noreply.github.com>
2023-09-18 19:50:57 +0200

    Add files via upload

---
+++ FSH_filter/FSH_filter/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace FSH_filter
+{
+    partial class fsh_filter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fsh_filter));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cre_by = new System.Windows.Forms.Label();
+            this.ok_bt = new System.Windows.Forms.Button();
+            this.line_p = new System.Windows.Forms.PictureBox();
+            this.file_type_tb = new System.Windows.Forms.TextBox();
+            this.file_contain_tb = new System.Windows.Forms.TextBox();
+            this.file_type_bt = new System.Windows.Forms.Button();
+            this.file_contain_bt = new System.Windows.Forms.Button();
+            this.file_type_l = new System.Windows.Forms.Label();
+            this.file_contain_l = new System.Windows.Forms.Label();
+            this.inf_type_l = new System.Windows.Forms.Label();
+            this.inf_contain_l = new System.Windows.Forms.Label();
+            this.file_type_bx = new System.Windows.Forms.TextBox();
+            this.file_cont_bx = new System.Windows.Forms.TextBox();
+            this.hide_p = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line_p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hide_p)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cre_by
+            // 
+            this.cre_by.AutoSize = true;
+            this.cre_by.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.cre_by.Location = new System.Drawing.Point(38, 185);
+            this.cre_by.Name = "cre_by";
+            this.cre_by.Size = new System.Drawing.Size(461, 14);
+            this.cre_by.TabIndex = 1;
+            this.cre_by.Text = "Folder Structer Handler v0.0.1 - Copyright © 2023 by D0M4K0M4. All Rights Reserve" +
+    "d";
+            // 
+            // ok_bt
+            // 
+            this.ok_bt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ok_bt.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold);
+            this.ok_bt.Location = new System.Drawing.Point(574, 175);
+            this.ok_bt.Name = "ok_bt";
+            this.ok_bt.Size = new System.Drawing.Size(86, 26);
+            this.ok_bt.TabIndex = 2;
+            this.ok_bt.Text = "OK";
+            this.ok_bt.UseVisualStyleBackColor = true;
+            this.ok_bt.Click += new System.EventHandler(this.ok_bt_Click);
+            // 
+            // line_p
+            // 
+            this.line_p.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("line_p.BackgroundImage")));
+            this.line_p.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.line_p.Location = new System.Drawing.Point(12, 84);
+            this.line_p.Name = "line_p";
+            this.line_p.Size = new System.Drawing.Size(548, 10);
+            this.line_p.TabIndex = 3;
+            this.line_p.TabStop = false;
+            // 
+            // file_type_tb
+            // 
+            this.file_type_tb.BackColor = System.Drawing.SystemColors.Window;
+            this.file_type_tb.Location = new System.Drawing.Point(12, 35);
+            this.file_type_tb.Name = "file_type_tb";
+            this.file_type_tb.Size = new System.Drawing.Size(456, 20);
+            this.file_type_tb.TabIndex = 4;
+            // 
+            // file_contain_tb
+            // 
+            this.file_contain_tb.Location = new System.Drawing.Point(12, 122);
+            this.file_contain_tb.Name = "file_contain_tb";
+            this.file_contain_tb.Size = new System.Drawing.Size(456, 20);
+            this.file_contain_tb.TabIndex = 5;
+            // 
+            // file_type_bt
+            // 
+            this.file_type_bt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.file_type_bt.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.file_type_bt.Location = new System.Drawing.Point(474, 33);
+            this.file_type_bt.Name = "file_type_bt";
+            this.file_type_bt.Size = new System.Drawing.Size(86, 23);
+            this.file_type_bt.TabIndex = 6;
+            this.file_type_bt.Text = "SAVE";
+            this.file_type_bt.UseVisualStyleBackColor = true;
+            this.file_type_bt.Click += new System.EventHandler(this.file_type_bt_Click);
+            // 
+            // file_contain_bt
+            // 
+            this.file_contain_bt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.file_contain_bt.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.file_contain_bt.Location = new System.Drawing.Point(474, 120);
+            this.file_contain_bt.Name = "file_contain_bt";
+            this.file_contain_bt.Size = new System.Drawing.Size(86, 23);
+            this.file_contain_bt.TabIndex = 7;
+            this.file_contain_bt.Text = "SAVE";
+            this.file_contain_bt.UseVisualStyleBackColor = true;
+            this.file_contain_bt.Click += new System.EventHandler(this.file_contain_bt_Click);
+            // 
+            // file_type_l
+            // 
+            this.file_type_l.AutoSize = true;
+            this.file_type_l.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.file_type_l.Location = new System.Drawing.Point(9, 18);
+            this.file_type_l.Name = "file_type_l";
+            this.file_type_l.Size = new System.Drawing.Size(519, 14);
+            this.file_type_l.TabIndex = 8;
+            this.file_type_l.Text = "File extension filtering: (separate extensions with \',\') - if it is empty it will" +
+    " be no filtering apply";
+            // 
+            // file_contain_l
+            // 
+            this.file_contain_l.AutoSize = true;
+            this.file_contain_l.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.file_contain_l.Location = new System.Drawing.Point(9, 105);
+            this.file_contain_l.Name = "file_contain_l";
+            this.file_contain_l.Size = new System.Drawing.Size(504, 14);
+            this.file_contain_l.TabIndex = 9;
+            this.file_contain_l.Text = "File contain filtering: (separate extensions with \',\') - if it is empty it will b" +
+    "e no filtering apply";
+            // 
+            // inf_type_l
+            // 
+            this.inf_type_l.AutoSize = true;
+            this.inf_type_l.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.inf_type_l.Location = new System.Drawing.Point(9, 58);
+            this.inf_type_l.Name = "inf_type_l";
+            this.inf_type_l.Size = new System.Drawing.Size(551, 14);
+            this.inf_type_l.TabIndex = 10;
+            this.inf_type_l.Text = "Write down some extension type(s) for filtering: In this example: .png, .txt, .jp" +
+    "g only this file types will be handled.";
+            // 
+            // inf_contain_l
+            // 
+            this.inf_contain_l.AutoSize = true;
+            this.inf_contain_l.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.inf_contain_l.Location = new System.Drawing.Point(9, 145);
+            this.inf_contain_l.Name = "inf_contain_l";
+            this.inf_contain_l.Size = new System.Drawing.Size(646, 14);
+            this.inf_contain_l.TabIndex = 11;
+            this.inf_contain_l.Text = "Write down some text components for filtering: In this example: car, 1, dog only " +
+    "files handled that is contains one of this components.";
+            // 
+            // file_type_bx
+            // 
+            this.file_type_bx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.file_type_bx.Location = new System.Drawing.Point(574, 18);
+            this.file_type_bx.Multiline = true;
+            this.file_type_bx.Name = "file_type_bx";
+            this.file_type_bx.ReadOnly = true;
+            this.file_type_bx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.file_type_bx.Size = new System.Drawing.Size(86, 54);
+            this.file_type_bx.TabIndex = 12;
+            this.file_type_bx.TextChanged += new System.EventHandler(this.file_type_bx_TextChanged);
+            // 
+            // file_cont_bx
+            // 
+            this.file_cont_bx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.file_cont_bx.Location = new System.Drawing.Point(574, 84);
+            this.file_cont_bx.Multiline = true;
+            this.file_cont_bx.Name = "file_cont_bx";
+            this.file_cont_bx.ReadOnly = true;
+            this.file_cont_bx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.file_cont_bx.Size = new System.Drawing.Size(86, 58);
+            this.file_cont_bx.TabIndex = 13;
+            this.file_cont_bx.TextChanged += new System.EventHandler(this.file_cont_bx_TextChanged);
+            // 
+            // hide_p
+            // 
+            this.hide_p.Location = new System.Drawing.Point(642, 18);
+            this.hide_p.Name = "hide_p";
+            this.hide_p.Size = new System.Drawing.Size(18, 124);
+            this.hide_p.TabIndex = 14;
+            this.hide_p.TabStop = false;
+            // 
+            // fsh_filter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 213);
+            this.Controls.Add(this.hide_p);
+            this.Controls.Add(this.file_cont_bx);
+            this.Controls.Add(this.file_type_bx);
+            this.Controls.Add(this.inf_contain_l);
+            this.Controls.Add(this.inf_type_l);
+            this.Controls.Add(this.file_contain_l);
+            this.Controls.Add(this.file_type_l);
+            this.Controls.Add(this.file_contain_bt);
+            this.Controls.Add(this.file_type_bt);
+            this.Controls.Add(this.file_contain_tb);
+            this.Controls.Add(this.file_type_tb);
+            this.Controls.Add(this.line_p);
+            this.Controls.Add(this.ok_bt);
+            this.Controls.Add(this.cre_by);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fsh_filter";
+            this.Text = "FSH";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.line_p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hide_p)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label cre_by;
+        private System.Windows.Forms.Button ok_bt;
+        private System.Windows.Forms.PictureBox line_p;
+        private System.Windows.Forms.TextBox file_type_tb;
+        private System.Windows.Forms.TextBox file_contain_tb;
+        private System.Windows.Forms.Button file_type_bt;
+        private System.Windows.Forms.Button file_contain_bt;
+        private System.Windows.Forms.Label file_type_l;
+        private System.Windows.Forms.Label file_contain_l;
+        private System.Windows.Forms.Label inf_type_l;
+        private System.Windows.Forms.Label inf_contain_l;
+        private System.Windows.Forms.TextBox file_type_bx;
+        private System.Windows.Forms.TextBox file_cont_bx;
+        private System.Windows.Forms.PictureBox hide_p;
+    }
+}
+
---
+++ FSH_filter/FSH_filter/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FSH_filter
+{
+    public partial class fsh_filter : Form
+    {
+        private string[] file_type = null;
+        private string[] file_containing = null;
+        private string dataPth = "settings.json";
+        private string checksideprogram = "Folder Structer Handler (FSH).exe";
+
+        //Dll imp
+
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        public static extern void DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attribute, ref int pvAttribute, uint cbAttribute);
+
+        public enum DWMWINDOWATTRIBUTE : uint
+        {
+            DWMWA_NCRENDERING_ENABLED,
+            DWMWA_NCRENDERING_POLICY,
+            DWMWA_TRANSITIONS_FORCEDISABLED,
+            DWMWA_ALLOW_NCPAINT,
+            DWMWA_CAPTION_BUTTON_BOUNDS,
+            DWMWA_NONCLIENT_RTL_LAYOUT,
+            DWMWA_FORCE_ICONIC_REPRESENTATION,
+            DWMWA_FLIP3D_POLICY,
+            DWMWA_EXTENDED_FRAME_BOUNDS,
+            DWMWA_HAS_ICONIC_BITMAP,
+            DWMWA_DISALLOW_PEEK,
+            DWMWA_EXCLUDED_FROM_PEEK,
+            DWMWA_CLOAK,
+            DWMWA_CLOAKED,
+            DWMWA_FREEZE_REPRESENTATION,
+            DWMWA_PASSIVE_UPDATE_MODE,
+            DWMWA_USE_HOSTBACKDROPBRUSH,
+            DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33,
+            DWMWA_BORDER_COLOR,
+            DWMWA_CAPTION_COLOR,
+            DWMWA_TEXT_COLOR,
+            DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
+            DWMWA_SYSTEMBACKDROP_TYPE,
+            DWMWA_LAST
+        }
+
+        public fsh_filter()
+        {
+            InitializeComponent();
+            designSet();
+            this.Show();
+
+            //check_exist 
+              if (!File.Exists(checksideprogram))
+              {
+                  MessageBox.Show("Error: 'Folder Structer Handler (FSH).exe' not found. The application will terminate.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  Environment.Exit(0); // Az alkalmazás leállítása
+              }
+            if (!File.Exists(dataPth))
+            {
+                MessageBox.Show("Error: 'settings.json' not found. The application will terminate.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0); // Az alkalmazás leállítása
+            }
+            else
+            {
+                string json_entry = File.ReadAllText(dataPth);
+                dynamic settings = JsonConvert.DeserializeObject(json_entry);
+
+                // Elolvassa a JSON tömböket JArray objektumokként
+                JArray fileTypesArray = (JArray)settings["grab_file_types"];
+                JArray fileContainingArray = (JArray)settings["grab_file_containing"];
+
+                // Konvertálja a JArray objektumokat string tömbbé
+                file_type = fileTypesArray.Select(item => (string)item).ToArray();
+                file_containing = fileContainingArray.Select(item => (string)item).ToArray();
+                dsp_ext(file_type);
+                dsp_cont(file_containing);
+            }
+        }
+
+        public void designSet()
+        {
+            Color sysgray = Color.FromArgb(25, 25, 25);
+            Color compgray = Color.FromArgb(51, 51, 51);
+
+            this.Icon = new Icon(AppDomain.CurrentDomain.BaseDirectory + @"\assets\fsh_filter.ico");
+            this.Text = "Folder Structure Handler - Filtering";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            this.BackColor = sysgray;
+            var preference = Convert.ToInt32(true);
+            DwmSetWindowAttribute(this.Handle, DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE, ref preference, sizeof(uint));
+
+            //Set components
+            file_type_bx.Cursor = new Cursor(AppDomain.CurrentDomain.BaseDirectory + @"\assets\cursor_scroll.ico");
+            file_cont_bx.Cursor = new Cursor(AppDomain.CurrentDomain.BaseDirectory + @"\assets\cursor_scroll.ico");
+            hide_p.BackColor = sysgray;
+            file_type_bx.BackColor = sysgray;
+            file_cont_bx.BackColor = sysgray;
+            file_type_bx.ForeColor = Color.Green;
+            file_cont_bx.ForeColor = Color.Green;
+            file_type_tb.BackColor = compgray;
+            file_contain_tb.BackColor = compgray;
+            file_type_tb.ForeColor = Color.White;
+            file_contain_tb.ForeColor = Color.White;
+            cre_by.ForeColor = compgray;
+            file_type_l.ForeColor = Color.White;
+            file_contain_l.ForeColor = Color.White;
+            inf_type_l.ForeColor = Color.White;
+            inf_contain_l.ForeColor = Color.White;
+        }
+
+        public void dsp_ext(string[] raw_type)
+        {
+            file_type_bx.AppendText("Applied type:" + Environment.NewLine);
+            int step = 0;
+
+            if (raw_type.Length == 0)
+            {
+                file_type_bx.AppendText("'None'");
+            }
+            else
+            {
+                foreach (string type in raw_type)
+                {
+                    if (step == 0)
+                    {
+                        file_type_bx.AppendText(type);
+                    }
+                    else
+                    {
+                        file_type_bx.AppendText(", " + type);
+                    }
+                    step++;
+                }
+            }
+        }
+
+        public void dsp_cont(string[] raw_cont)
+        {
+            file_cont_bx.AppendText("Applied comp:" + Environment.NewLine);
+            int step = 0;
+
+            if (raw_cont.Length == 0)
+            {
+                file_cont_bx.AppendText("'None'");
+            }
+            else
+            {
+                foreach (string type in raw_cont)
+                {
+                    if (step == 0)
+                    {
+                        file_cont_bx.AppendText(type);
+                    }
+                    else
+                    {
+                        file_cont_bx.AppendText(", " + type);
+                    }
+                    step++;
+                }
+            }
+        }
+
+        private void file_type_bt_Click(object sender, EventArgs e)
+        {
+            string json_type = File.ReadAllText(dataPth);
+            dynamic settings_type = JsonConvert.DeserializeObject(json_type);
+            JArray fileTypes = (JArray)settings_type["grab_file_types"];
+
+            string file_type_value = file_type_tb.Text;
+            string[] file_ext_value = file_type_value.Split(',');
+
+            if (file_type_value.Length == 0)
+            {
+                fileTypes.Clear();
+                string updatedJson = JsonConvert.SerializeObject(settings_type, Formatting.Indented);
+                File.WriteAllText(dataPth, updatedJson);
+                file_type_bx.Text = null;
+                file_type_bx.AppendText("Applied type:" + Environment.NewLine);
+                file_type_bx.AppendText("'None'");
+                inf_type_l.Text = "File type extension: 'none' applied...";
+                inf_type_l.ForeColor = Color.Green;
+            }
+            else if (file_type_value.Length > 0)
+            {
+                string file_types = null;
+                int step = 0;
+                bool hasError = false;
+                fileTypes.Clear();
+                foreach (string ext_value in file_ext_value)
+                {
+                    string ext_type = ext_value.Trim();
+
+                    if (!ext_type.Contains('.') || ext_type.Length == 0)
+                    {
+                        hasError = true;
+                    }
+
+                    fileTypes.Add(ext_type);
+
+                    if (step == 0)
+                    {
+                        file_types += ext_type;
+                    }
+                    else
+                    {
+                        file_types += ", " + ext_type;
+                    }
+                    step++;
+                }
+                if (hasError)
+                {
+                    file_type_bx.Text = null;
+                    file_type_bx.AppendText("Applied type:" + Environment.NewLine);
+                    file_type_bx.AppendText("'None'");
+                    inf_type_l.Text = "Some of the contains are empty or the extension(s) doesn't contain '.'!";
+                    inf_type_l.ForeColor = Color.Red;
+                    file_type_tb.Text = null;
+                }
+                else if (!hasError)
+                {
+                    file_type_bx.Text = null;
+                    file_type_bx.AppendText("Applied type:" + Environment.NewLine);
+                    file_type_bx.AppendText(file_types + Environment.NewLine);
+                    string updatedJson = JsonConvert.SerializeObject(settings_type, Formatting.Indented);
+                    File.WriteAllText(dataPth, updatedJson);
+                    inf_type_l.Text = $"File type extension: '{file_types}' applied...";
+                    inf_type_l.ForeColor = Color.Green;
+                    file_type_tb.Text = null;
+                }
+            }
+        }
+
+        private void file_contain_bt_Click(object sender, EventArgs e)
+        {
+            string json_contain = File.ReadAllText(dataPth);
+            dynamic settings_cont = JsonConvert.DeserializeObject(json_contain);
+            JArray fileCont = (JArray)settings_cont["grab_file_containing"];
+
+            string file_contain_value = file_contain_tb.Text;
+            string[] file_comp_value = file_contain_value.Split(',');
+
+            if (file_contain_value.Length == 0)
+            {
+                fileCont.Clear();
+                string updatedJson = JsonConvert.SerializeObject(settings_cont, Formatting.Indented);
+                File.WriteAllText(dataPth, updatedJson);
+                file_cont_bx.Text = null;
+                file_cont_bx.AppendText("Applied type:" + Environment.NewLine);
+                file_cont_bx.AppendText("'None'");
+                inf_contain_l.Text = "File contain(s): 'none' applied...";
+                inf_contain_l.ForeColor = Color.Green;
+            }
+            else if (file_contain_value.Length > 0)
+            {
+                string file_comps = null;
+                int step = 0;
+                bool hasError = false;
+                fileCont.Clear();
+                foreach (string comp_value in file_comp_value)
+                {
+                    string comp_type = comp_value.Trim();
+
+                    if (comp_type.Length == 0)
+                    {
+                        hasError = true;
+                    }
+
+                    fileCont.Add(comp_type);
+
+                    if (step == 0)
+                    {
+                        file_comps += comp_type;
+                    }
+                    else
+                    {
+                        file_comps += ", " + comp_type;
+                    }
+                    step++;
+                }
+                if (hasError)
+                {
+                    file_cont_bx.Text = null;
+                    file_cont_bx.AppendText("Applied type:" + Environment.NewLine);
+                    file_cont_bx.AppendText("'None'");
+                    inf_contain_l.Text = "Some of the contain(s) are empty!";
+                    inf_contain_l.ForeColor = Color.Red;
+                    file_contain_tb.Text = null;
+                }
+                else if (!hasError)
+                {
+                    file_cont_bx.Text = null;
+                    file_cont_bx.AppendText("Applied type:" + Environment.NewLine);
+                    file_cont_bx.AppendText(file_comps + Environment.NewLine);
+                    string updatedJson = JsonConvert.SerializeObject(settings_cont, Formatting.Indented);
+                    File.WriteAllText(dataPth, updatedJson);
+                    inf_contain_l.Text = $"File contain(s): '{file_comps}' applied...";
+                    inf_contain_l.ForeColor = Color.Green;
+                    file_contain_tb.Text = null;
+                }
+            }
+        }
+
+        private void ok_bt_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void file_type_bx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void file_cont_bx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
